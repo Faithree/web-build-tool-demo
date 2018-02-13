781d4365646867b23daa71ae52c83492ace6ca61
Faithree <359790031@qq.com>
2018-02-13 16:17:04 +0800

    03-代码抽离

---
+++ 03-conmonChunk/src/index.js
@@ -0,0 +1,5 @@
+import c from './c'
+// import vue from 'vue'
+document.write('<h1>乘风gg</h1>  ')
+let func = () => {}
+console.log([1, 2, 3].includes(3, 3));  // false
---
+++ 03-conmonChunk/webpack.config.js
@@ -0,0 +1,42 @@
+const webpack = require('webpack')
+module.exports = {
+  entry: {
+    index: __dirname + '/src/index.js',
+    admin: __dirname + '/src/admin.js',
+    vendor: ['vue', 'lodash']
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: '[name].[chunkhash].bundle.js',
+    chunkFilename: "[name].chunk.js"
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['babel-preset-env', {
+              targets: {
+                browsers: ['>1%', 'last 2 versions']
+              }
+            }]
+          ]
+        }
+      },
+      exclude: '/node_modules/'
+    }]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'common',
+      minChunks: 2,
+      chunks: ['index', 'admin']
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest'],
+      minChunks: Infinity
+    })
+  ]
+}
