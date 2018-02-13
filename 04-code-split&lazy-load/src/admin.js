db83af4109711559063ffd2704541c12675932c9
Faithree <359790031@qq.com>
2018-02-13 20:28:21 +0800

    懒加载

---
+++ 04-code-split&lazy-load/src/admin.js
@@ -0,0 +1,6 @@
+// const c = () => import(./c')
+const b = () => import(/* webpackChunkName: "async-admin" */'./b')
+
+import lodash from 'lodash'
+document.write('<h1>admin</h1>  ')
+
---
+++ 04-code-split&lazy-load/src/index.js
@@ -0,0 +1,7 @@
+const c = () => import(/* webpackChunkName: "async-index" */'./c')
+
+import vue from 'vue'
+new vue({})
+document.write('<h1>乘风gg</h1>  ')
+let func = () => {}
+console.log([1, 2, 3].includes(3, 3));  // false
