87c3bd44b3c1f66388cb26e71e4db44672d88e5b
Faithree <359790031@qq.com>
2018-02-14 17:20:41 +0800

    tree-shaking

---
+++ 08-tree-shaking/src/index.js
@@ -0,0 +1,14 @@
+// import './css/reset.less'
+import base from './css/base.css'
+import {a} from './util'
+import {chunk} from 'lodash'
+
+chunk([1, 2, 3, 4, 5, 6, 7])
+a()
+//
+// var h2Ele = document.createElement('h2')
+// h2Ele.className = 'border'
+// var bodyEle = document.getElementsByClassName('body')
+// bodyEle.appendChild(h2Ele)
+document.write('<h2 class="border">乘风gg</h2>')
+
---
+++ 08-tree-shaking/src/util.js
@@ -0,0 +1,12 @@
+export function a () {
+  console.log('i am a')
+}
+
+export function b () {
+  console.log('i am b')
+}
+
+export function c () {
+  console.log('i am c')
+}
+
