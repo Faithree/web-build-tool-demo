7b77c63374a69e59ce4275d576d9d845498f87a6
Faithree <359790031@qq.com>
2018-02-14 15:59:10 +0800

    postcss

---
+++ 07-postCss/src/index.js
@@ -0,0 +1,7 @@
+import base from './css/base.css'
+import './css/reset.less'
+
+document.write(`<h1 class="${base.border}">乘风gg</h1>  `)
+let func = () => {
+}
+console.log([1, 2, 3].includes(3, 3));  // false
