c30433b37731bc7c5e89a687bceec23562291e0a
Faithree <359790031@qq.com>
2018-02-21 09:21:47 +0800

    web-dev-server

---
+++ 12-web-dev-server/src/index.js
@@ -0,0 +1,13 @@
+// import './css/reset.less'
+import base from './css/base.css'
+//
+var h2Ele = document.createElement('h3')
+h2Ele.className = 'border'
+var bodyEle = document.querySelector('body')
+bodyEle.appendChild(h2Ele)
+// document.write('<h2 class="border">乘风gg</h2>')
+
+if (module.hot) {
+  console.log(1111111)
+  module.hot.accept()
+}
