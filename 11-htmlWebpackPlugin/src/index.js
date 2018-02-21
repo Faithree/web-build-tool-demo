f2ed55a836d3883b4f76c1f53b0e79475abb5999
Faithree <359790031@qq.com>
2018-02-21 09:20:54 +0800

    htmlWebpackPlugin

---
+++ 11-htmlWebpackPlugin/src/index.js
@@ -0,0 +1,9 @@
+// import './css/reset.less'
+import base from './css/base.css'
+//
+// var h2Ele = document.createElement('h2')
+// h2Ele.className = 'border'
+// var bodyEle = document.getElementsByClassName('body')
+// bodyEle.appendChild(h2Ele)
+document.write('<h2 class="border">乘风gg</h2>')
+
---
+++ 11-htmlWebpackPlugin/webpack.config.js
@@ -0,0 +1,126 @@
+const webpack = require('webpack')
+const extractTextWebpackPlugin = require('extract-text-webpack-plugin')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+function resolve (dir) {
+  return path.join(__dirname, dir)
+}
+module.exports = {
+  entry: {
+    index: __dirname + '/src/index.js'
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.[hash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: extractTextWebpackPlugin.extract({
+          fallback: {
+            loader: 'style-loader',
+            /* options: {
+               insertInto: 'body',//插入到哪个dom上面
+               singletom: true, // 把所有的style合成一个
+               transform: './css.transform.js' // 类似钩子，发生在浏览器环境，可以根据浏览器环境不同做出不同的兼容，例如做media query
+             }*/
+          },
+          use: [{
+            loader: 'css-loader',
+            options: {
+              minimize: true,// 开启压缩
+              // module: true// 模块化
+            },
+            // loader: 'file-loader',
+          },
+            {
+              loader: 'postcss-loader',
+              options: {
+                ident: 'postcss',
+                plugins: [
+                  // require('autoprefixer')(),
+                  // require('cssnano')(),
+                  // require('postcss-cssnext')()
+                ]
+              }
+            },]
+        })
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            name: '[name].min.[ext]',
+            limit: 5000,
+            // publicPath: 'dist/',
+            outputPath: 'img/',
+            // useRelativePath: true
+          }
+        },
+          {
+            loader: 'img-loader',
+            options: {
+              gifsicle: {
+                interlaced: false
+              },
+              mozjpeg: {
+                progressive: true,
+                arithmetic: false
+              },
+              optipng: false, // disabled
+              pngquant: {
+                floyd: 0.5,
+                speed: 2
+              },
+              svgo: {
+                plugins: [
+                  {removeTitle: true},
+                  {convertPathData: false}
+                ]
+              }
+            }
+          },
+          // {
+          //   loader: 'postcss-loader',
+          //   options: {
+          //     ident: 'postcss',
+          //     plugins: [
+          //       require('postcss-sprites')({
+          //         spritePath: 'dist/img/sprites',
+          //         retina: true
+          //       })
+          //     ]
+          //   }
+          // }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              publicPath: '',
+              attrs: ['img:src', 'img:data-src']
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    // new ExtractTextPlugin("[name].[contenthash:8].css"),
+    new extractTextWebpackPlugin({
+      filename: '[name].[hash].min.css',
+      allChunks: false // 制定提取css的范围,提取初始化（非异步加载）,此时在commonChunk插件下，css也会被当成一个chunk,所有要用contenthash
+    }),
+    new htmlWebpackPlugin({
+      filename: 'index.html',
+      template: './index.html',
+      inject: true
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  ]
+}
