5b798702814b4695ca0a086cd3b75a4ee7b5b0fe
Faithree <359790031@qq.com>
2018-02-23 10:27:05 +0800

    vue打包优化

---
+++ 17-vue打包优化/build/webpack.dll.conf.js
@@ -0,0 +1,20 @@
+const path = require('path')
+const webpack = require('webpack')
+module.exports = {
+  entry: {
+    vue: ['vue', 'vue-router'],
+    ui: ['element-ui']
+  },
+  output: {
+    path: path.join(__dirname, '../src/dll/'),
+    filename: "[name].dll.js",
+    library: '[name]'
+  },
+  plugins: [
+    new webpack.DllPlugin({
+      path: path.join(__dirname, '../src/dll', '[name]-manifest.json'),
+      name: '[name]'
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  ]
+}
