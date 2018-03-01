2bc8e5e1c949e372d5a6b071cb1d03fa5d519375
Faithree <359790031@qq.com>
2018-03-01 20:05:34 +0800

    webpack4.0初步试用

---
+++ 19-webpack4.0新特性/src/index.js
@@ -0,0 +1,3 @@
+let ModuleA = require('./moduleA.js')
+console.log(ModuleA)
+document.write('<h1>乘风gg</h1>  ')
---
+++ 19-webpack4.0新特性/src/moduleA.js
@@ -0,0 +1,2 @@
+module.exports = 'i am ModuleAAAAAAAAAAAAAAAAAA'
+console.log('我被执行了');
