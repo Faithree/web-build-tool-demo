e4ccac6268f7ad3936f2469bc4774b3f867e5e92
Faithree <359790031@qq.com>
2018-05-30 18:21:32 +0800

    懒加载

--- 21-splitChunk多页面/pageA.js
+++ 21-splitChunk多页面/pageA.js
@@ -2,4 +2,9 @@ var vue = require('vue');
 var utility1 = require('./utility1');
 var utility2 = require('./utility2');
 new Vue()
+
+import(/* webpackChunkName: "common-async.js" */"./common-async").then(common => {
+  console.log(common);
+})
+
 module.exports = "pageA";
--- 21-splitChunk多页面/pageB.js
+++ 21-splitChunk多页面/pageB.js
@@ -2,5 +2,7 @@ var react = require('react');
 var reactDom = require('react-dom');
 var utility2 = require('./utility2');
 var utility3 = require('./utility3');
-
+import(/* webpackChunkName: "common-async.js" */"./common-async").then(common => {
+  console.log(common);
+})
 module.exports = "pageB";
